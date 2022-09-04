3f955dbaa72b26a50316d3f865ebcecd45923876
was <alison.simiao@gmail.com>
2022-09-04 18:03:04 -0300

    add database initialization

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import express from "express";
+import db from "./database/db";
 
 const app = express();
 
-
-app.listen(3000, ()=>{
+app.listen(3000, async ()=>{
+  await db.sync();
   console.log(`${process.env.PROJECT_NAME} running in port 3000`);
 })
