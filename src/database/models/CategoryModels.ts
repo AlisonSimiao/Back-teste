7dcfc93ac57bcf7aa1893882ff03d88296163574
was <alison.simiao@gmail.com>
2022-09-04 17:59:49 -0300

    create category models

---
+++ src/database/models/CategoryModels.ts
@@ -0,0 +1,24 @@
+import { DataTypes } from "sequelize/types";
+import db from "../db";
+
+export const CategoryModel = db.define('Categorias',
+{
+  id :{
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  codigo:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  titulo:{
+      type: DataTypes.STRING,
+      allowNull: false,
+  },
+  status: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: true
+  },
+});
