8977d4f6a8a4bc8d04585c1cbfe65baef6d26302
was <alison.simiao@gmail.com>
2022-09-04 20:38:43 -0300

    create Stock model

---
+++ src/database/models/StockModel.ts
@@ -0,0 +1,34 @@
+import { DataTypes, Deferrable } from "sequelize";
+import db from "../db"
+import { ProductModel } from "./ProductModel";
+
+export const StockModel = db.define('Estoque',{
+  id:{
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  idProduto: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: ProductModel,
+      key: 'id',
+      deferrable: new Deferrable.INITIALLY_IMMEDIATE
+    }
+  },
+  quantidade:{
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  reserva: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  status: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+}
+);
