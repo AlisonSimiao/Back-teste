558c36dc043d399e14e78a3886d45d7230dc2786
was <alison.simiao@gmail.com>
2022-09-05 19:38:51 -0300

    add catcher errors database init

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,18 @@
 import express from "express";
+import { extensions } from "sequelize/types/utils/validator-extras";
 import db from "./database/db";
+import { CategoryModel } from "./database/models/CategoryModels";
+import { ProductModel } from "./database/models/ProductModel";
+import { produto } from "./types/produto";
 
 const app = express();
 
 app.listen(3000, async ()=>{
-  await db.sync();
+  try{
+    await db.sync();
+  }
+  catch(e){
+    console.log("db error", e);
+  }
   console.log(`${process.env.PROJECT_NAME} running in port 3000`);
 })
