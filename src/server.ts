104e2941ac1c705da9f3a4e65b4531d19a72155a
was <alison.simiao@gmail.com>
2022-09-11 15:52:30 -0300

    build api as module

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,10 @@
 import express, { json } from "express";
 import db from "./database/db";
-import router from "./router"
+import router from "./router";
 
 const app = express();
-app.use( json() );
+
+app.use(json());
 app.use( router );
 
-app.listen(3000, async ()=>{
-  try{
-    await db.sync();
-  }
-  catch(e){
-    console.log("db error", e);
-  }
-  console.log(`${process.env.PROJECT_NAME} running in port 3000`);
-})
+export default app;
